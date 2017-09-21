e4ef7d0732be09dd5c474b92aba4192c50ca39cb
nginnever <ginneversource@gmail.com>
2017-09-20 21:51:39 -0600

    update

--- contracts/XCAT.sol
+++ contracts/XCAT.sol
@@ -1,10 +1,45 @@
 pragma solidity ^0.4.15;
 
-
 /**
  * @title XCAT
  * @dev Ethereum locking script
  */
 contract XCAT {
+	address public locker;
+	bytes32 public hashedSecret;
+	uint256 public lockedFunds;
+	uint256 public lockTime;
 
+	function buy(bytes32 _hash, uint256 _lockTime) payable {
+		require(now < _lockTime);
+		require(_hash != 0);
+
+		lockTime = _lockTime;
+		lockedFunds = msg.value;
+		locker = msg.sender;
+	}
+
+	function initiate(bytes32 _hash, uint256 _lockTime) payable {
+		require(now < _lockTime);
+		require(_hash != 0);
+
+		locker = msg.sender;
+		lockedFunds = msg.value;
+		lockTime = _lockTime;
+		hashedSecret = _hash;
+
+	}
+
+	function releasePreimage(bytes _preimage) {
+		bytes32 test = sha256(sha256(_preimage));
+		if(test == hashedSecret){
+			msg.sender.transfer(lockedFunds);
+		}
+	}
+
+	function releaseTime() {
+		if(now >= lockTime) {
+			locker.transfer(lockedFunds);
+		}
+	}
 }
