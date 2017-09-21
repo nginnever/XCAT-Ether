e4decfe4c6b0e250776e6af416b4f7d7a0ea1c9b
Nathan Ginnever <ginneversource@gmail.com>
2017-09-20 21:56:16 -0600

    Update XCAT.sol

--- contracts/XCAT.sol
+++ contracts/XCAT.sol
@@ -5,41 +5,41 @@ pragma solidity ^0.4.15;
  * @dev Ethereum locking script
  */
 contract XCAT {
-	address public locker;
-	bytes32 public hashedSecret;
-	uint256 public lockedFunds;
-	uint256 public lockTime;
+  address public locker;
+  bytes32 public hashedSecret;
+  uint256 public lockedFunds;
+  uint256 public lockTime;
 
-	function buy(bytes32 _hash, uint256 _lockTime) payable {
-		require(now < _lockTime);
-		require(_hash != 0);
+  function buy(bytes32 _hash, uint256 _lockTime) payable {
+    require(now < _lockTime);
+    require(_hash != 0);
 
-		lockTime = _lockTime;
-		lockedFunds = msg.value;
-		locker = msg.sender;
-	}
+    lockTime = _lockTime;
+    lockedFunds = msg.value;
+    locker = msg.sender;
+  }
 
-	function initiate(bytes32 _hash, uint256 _lockTime) payable {
-		require(now < _lockTime);
-		require(_hash != 0);
+  function initiate(bytes32 _hash, uint256 _lockTime) payable {
+    require(now < _lockTime);
+    require(_hash != 0);
 
-		locker = msg.sender;
-		lockedFunds = msg.value;
-		lockTime = _lockTime;
-		hashedSecret = _hash;
+    locker = msg.sender;
+    lockedFunds = msg.value;
+    lockTime = _lockTime;
+    hashedSecret = _hash;
 
-	}
+    }
 
-	function releasePreimage(bytes _preimage) {
-		bytes32 test = sha256(sha256(_preimage));
-		if(test == hashedSecret){
-			msg.sender.transfer(lockedFunds);
-		}
-	}
+  function releasePreimage(bytes _preimage) {
+    bytes32 test = sha256(sha256(_preimage));
+    if(test == hashedSecret){
+      msg.sender.transfer(lockedFunds);
+    }
+  }
 
-	function releaseTime() {
-		if(now >= lockTime) {
-			locker.transfer(lockedFunds);
-		}
-	}
+  function releaseTime() {
+    if(now >= lockTime) {
+      locker.transfer(lockedFunds);
+    }
+  }
 }
